0f4c470119a880fe3240547ae2fc78f9b8f6e678
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-03 16:08:59 -0500

    Update hashingPassword in userModel

--- backEnd/controllers/userCtrl.js
+++ backEnd/controllers/userCtrl.js
@@ -1,5 +1,5 @@
 import User from "../models/userModel.js";
-import bcrypt from 'bcryptjs'
+
 
 // ----------------- REGISTER -----------------
 const register = async (req,res, next) => {
@@ -23,17 +23,18 @@ const register = async (req,res, next) => {
       return next('This account has already been registered')
     }
 
-    // Hash password and create new user
-    const hashPassword = bcrypt.hashSync(req.body.password, 10)
+    // Hash password at userModel
+    // const hashPassword = bcrypt.hashSync(req.body.password, 10)
     user = await User.create({
       name,
       email,
-      password: hashPassword
+      password: req.body.password
     })
     const {password, ...rest} = user._doc
     return res.status(201).json({success: true, message: 'Successful create the account', data: rest})
   } catch (error) {
-    return next('Interval server error')
+    res.status(500)
+    return next(error.message)
   }
 }
 
