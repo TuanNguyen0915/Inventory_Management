6eab5b08a3fe51bb4f7b62ddd1023126b0f9d682
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-03 22:39:07 -0500

    Create login functionality

--- backEnd/controllers/userCtrl.js
+++ backEnd/controllers/userCtrl.js
@@ -1,5 +1,11 @@
 import User from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
+// -------------- GENERATE TOKEN --------------
+const generateToken = (id) => {
+  return jwt.sign({id},process.env.JWT_SECRET, {expiresIn:'2d'})
+}
 
 // ----------------- REGISTER -----------------
 const register = async (req,res, next) => {
@@ -30,13 +36,55 @@ const register = async (req,res, next) => {
       email,
       password: req.body.password
     })
+
+    const token = generateToken(user._id)
     const {password, ...rest} = user._doc
-    return res.status(201).json({success: true, message: 'Successful create the account', data: rest})
+    // send http-only cookie
+    res.cookie('token', token, {
+      path: '/',
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000*60*60*24*2), // 2 days expires
+      sameSite: 'none',
+      secure: true 
+    })
+    return res.status(201).json({success: true, message: 'Successful create the account', data: rest, token})
   } catch (error) {
     res.status(500)
     return next(error.message)
   }
 }
 
+// ----------------- LOGIN -----------------
 
-export {register} 
+const login = async (req,res,next) => {
+  try {
+    let user = await User.findOne({email: req.body.email})
+    // check email
+    if (!user) {
+      res.status(401)
+      return next('This account does not register')
+    }
+    // check password
+    const isPasswordMatching = bcrypt.compareSync(req.body.password, user.password)
+    if(!isPasswordMatching) {
+      res.status(400)
+      return next('Password is not matching')
+    }
+    //create token and send to cookie
+    const token = generateToken(user._id)
+    res.cookie('token', token, {
+      path: '/',
+      httpOnly: true,
+      expires : new Date(Date.now() + 1000*60*60*24*2),
+      sameSite: 'none',
+      secure: true
+    })
+    const {password, ...rest} = user._doc
+    return res.status(200).json({success: true, message:'Login successful', data: rest, token})
+  } catch (error) {
+    res.status(500)
+    return next(error.message)
+  }
+}
+
+export {register, login} 
